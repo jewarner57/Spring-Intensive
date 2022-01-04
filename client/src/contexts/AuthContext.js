12b1614fd1add691be77cf0297e08ec1ab292ed9
Jonathan Warner <jewarner57@gmail.com>
2022-01-03 19:23:32 -0500

    feat: user can create an account

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,4 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
+import Cookies from 'js-cookie';
+
 const AuthContext = React.createContext()
 
 export function useAuth() {
@@ -8,8 +10,30 @@ export function useAuth() {
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState()
 
-  function signup(email, password) {
+  async function signup(email, password) {
     // signup user
+    try {
+      const rawResponse = await fetch(`${process.env.REACT_APP_API_URL}/user/signup`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+      });
+      const content = await rawResponse.json();
+
+      // If the response is not 200 throw an error
+      if (rawResponse.status !== 200) {
+        throw new Error(content.err)
+      }
+
+      setCurrentUser(content.user)
+    }
+    catch (err) {
+      // console.log(err)
+      throw new Error(err.message)
+    }
   }
 
   async function login(email, password, rememberMe) {
--- client/src/pages/SignUpPage/index.js
+++ client/src/pages/SignUpPage/index.js
@@ -1,15 +1,30 @@
 import React, { useState } from 'react';
+import { useAuth } from '../../contexts/AuthContext';
+import { useNavigate } from 'react-router';
 import './style.css'
 
 
 export default function SignupPage(props) {
   const [email, setEmail] = useState()
   const [password, setPassword] = useState()
+  const [error, setError] = useState()
+  const { signup } = useAuth()
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate();
 
-  const handleFormSubmit = (e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault()
-    console.log(email, password)
+    setLoading(true)
 
+    try {
+      await signup(email, password)
+      setLoading(false)
+      navigate('/#/')
+    }
+    catch (err) {
+      setError(err.message)
+      setLoading(false)
+    }
   }
 
   return (
@@ -24,7 +39,8 @@ export default function SignupPage(props) {
 
             <label htmlFor="email">Password *</label>
             <input id="password" type="password" className="form-field" val={password} onChange={(e) => setPassword(e.target.value)} />
-            <button className="submit-button">SIGNUP NOW</button>
+            {error ? <p className="error-text">{error}</p> : ""}
+            <button className="submit-button" disabled={loading}>SIGNUP NOW</button>
           </form >
         </div>
       </div>
