3ace858a1ba8ed12a5630dcc68bfffe97aaf0b37
Jonathan Warner <jewarner57@gmail.com>
2022-01-08 13:05:36 -0500

    feat: style 404 page

--- client/src/pages/Error404Page/index.js
+++ client/src/pages/Error404Page/index.js
@@ -1,9 +1,12 @@
 import './style.css';
+import image404 from '../../images/404-image.svg'
 
 function Error404Page() {
   return (
-    <div>
-      <h1>404: Page Not Found</h1>
+    <div className='error-404'>
+      <h1 className="title-404">404: Page Not Found</h1>
+      <p className="desc-404">We searched everywhere, but couldn't find the page you're looking for.</p>
+      <img src={image404} alt="404 error" />
     </div>
   );
 }
