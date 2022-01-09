2265f04217f7da2ba743f1a7e4fc4878fb932b7c
Jonathan Warner <jewarner57@gmail.com>
2022-01-09 13:18:20 -0500

    feat: start refactor of use api fetch code into a custom hook

---
+++ client/src/components/useApi/index.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react"
+import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate, useLocation } from "react-router-dom";
+import Error404Page from '../../pages/Error404Page'
+
+const useApi = (url, options = { method: 'GET', credentials: 'include' }) => {
+  const { clearUser } = useAuth()
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState({})
+
+  const fetchApi = async () => {
+    setError()
+    setLoading(true)
+
+    try {
+      const res = await fetch(url, {
+        method: 'GET',
+        credentials: 'include',
+      })
+
+      const content = await res.json();
+
+      // login redirect
+      if (res.status === 401 || res.status === 403) {
+        clearUser()
+        navigate('/signin', { next: location.pathname })
+      }
+
+      // 404 page
+      if (res.status === 404) {
+        setError(<Error404Page />)
+      }
+
+      // If the response is not 200 throw an error
+      if (res.status !== 200) {
+        setError(res.msg)
+      }
+
+      // set the content and leave loading state
+      setData(content)
+      setLoading(false)
+    }
+    catch (err) {
+      setLoading(false)
+      setError(err.message)
+      throw new Error(err.message)
+    }
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  return { loading, error, data }
+};
+
+export default useApi;
--- client/src/pages/ViewPost/index.js
+++ client/src/pages/ViewPost/index.js
@@ -1,54 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import LoadingCircle from '../../components/LoadingCircle';
-import Error404Page from '../Error404Page';
-import { useAuth } from '../../contexts/AuthContext';
-import { useNavigate, useParams, Link } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import './style.css';
 import CommentList from '../../components/CommentList';
 import ED from '@jewarner57/easydate'
+import useApi from '../../components/useApi';
 
 export default function ViewPost() {
-  const [loading, setLoading] = useState(true)
-  const [postContent, setPostContent] = useState({})
-  const [error, setError] = useState()
   const { id } = useParams();
-  const { clearUser } = useAuth()
-  const navigate = useNavigate()
+  const { loading, error, data: { media } } = useApi(`${process.env.REACT_APP_API_URL}/media/get/${id}`)
 
   useEffect(() => {
     window.scrollTo(0, 0)
-    getPost()
   }, [id])
-
-  const getPost = async () => {
-    setError()
-    setLoading(true)
-    try {
-      const res = await fetch(`${process.env.REACT_APP_API_URL}/media/get/${id}`, {
-        method: 'GET',
-        credentials: 'include'
-      })
-
-      const content = await res.json();
-
-      if (res.status === 401) {
-        clearUser()
-        navigate('/signin')
-      }
-
-      // If the response is not 200 throw an error
-      if (res.status !== 200) {
-        setError(<Error404Page />)
-      }
-      // set the content and leave loading state
-      setPostContent(content.media)
-      setLoading(false)
-    }
-    catch (err) {
-      setLoading(false)
-      throw new Error(err.message)
-    }
-  }
 
   return (
     <React.Fragment>
@@ -58,20 +22,20 @@ export default function ViewPost() {
             <LoadingCircle />
             :
             <div className="post-card">
-              <Link className="post-header" to={`/profile/${postContent.author._id}`}>
+              <Link className="post-header" to={`/profile/${media.author._id}`}>
                 <div className="button-primary">
-                  {postContent.author.username[0].toUpperCase()}
+                  {media.author.username[0].toUpperCase()}
                 </div>
                 <div className="post-header-info">
-                  <p className="post-content-header">{postContent.author.username[0].toUpperCase() + postContent.author.username.slice(1)}</p>
-                  <p className="post-content-date">{new ED(postContent.createdAt).format("%b %d, %Y")}</p>
+                  <p className="post-content-header">{media.author.username[0].toUpperCase() + media.author.username.slice(1)}</p>
+                  <p className="post-content-date">{new ED(media.createdAt).format("%b %d, %Y")}</p>
                 </div>
               </Link>
               <div className="post-image-wrapper">
-                <img src={`${process.env.REACT_APP_IPFS_READ_URL}${postContent.location}`} alt="Post Media" />
+                <img src={`${process.env.REACT_APP_IPFS_READ_URL}${media.location}`} alt="Post Media" />
               </div>
               <div className="post-content">
-                <CommentList post={postContent} commentCount={postContent.comments} />
+                <CommentList post={media} commentCount={media.comments} />
               </div>
             </div>
           }
