6d614ea62d8281440d1f62b48dcf90a5d5e93c11
Jonathan Warner <jewarner57@gmail.com>
2022-01-10 16:42:00 -0500

    fix: polish share button style

--- client/src/components/ShareButton/index.js
+++ client/src/components/ShareButton/index.js
@@ -33,7 +33,7 @@ export default function ShareButton(props) {
 
   return (
     <div className="share-button" onClick={() => setMenuOpen(prev => !prev)}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="22" fill="gray" viewBox="0 0 24 24"><path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z" /></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="22" fill={menuOpen ? '#dddddd' : 'gray'} viewBox="0 0 24 24"><path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z" /></svg>
       <ul className="share-dropdown" style={{ display: !menuOpen ? 'none' : '' }}>
         <li className="share-dropdown-item" onClick={() => copyLink()}>
           Copy Link
